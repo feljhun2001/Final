f6adcc371997ed63bb6122e2fe1c5bc1426f93ff
feljhun2001 <feloyfeloyfeloy@gmail.com>
2023-02-09 11:22:16 +0800

    Added Model and Migrate Table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,98 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
-use Illuminate\Validation\Rules;
-
-
-
-
 
 class HomeController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('homes.home', [
-            'header'    => 'Announcement',
-            'home'     => Product::all()
-        ]);
+        //
     }
 
-    public function form()
-    {
-        return view('homes.homeform', [
-            'header'    => 'Add Announcement',
-        ]);
-    }
-
-    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-         // For Validation
-         $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-        ]);
-
-        // For Storing after Validation
-        Product::create($request->all());
-
-        session()->flash('status', 'Added Announcement Successfully!');
-
-        // Redirect to the List of Users
-        return redirect('/home');
+        //
     }
 
-    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        $product = Product::find($id);
-
-        return view('homes.homeform', [
-                'header'    => 'Update User',
-                'home'      => $home
-            ]); 
+        //
     }
 
-    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
-         // For Validation
-         $request->validate($request->all);
-
-        $product = Product::find($id);
-
-        $product->update($request->all());
-
-        session()->flash('status', 'Updated User Successfully!');
-
-        return redirect('/home');
+        //
     }
 
-   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        $product = Product::find($id);
-        
-        return view('homes.homeform', [
-            'header'    => 'Delete Announcement',
-            'home'      => $product
-        ]);
-    }
-
-    public function delete(Request $request, $id)
-
-    {
-       
-        $product = Product::find($id);
-
-        $product->delete($request->all());
-
-        session()->flash('status', 'Deleted Announcement Successfully!');
-
-        return redirect('/home'); 
-
-
+        //
     }
 }
